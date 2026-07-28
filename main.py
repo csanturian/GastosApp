f29d9ddecac993a73769d09bbd50b83d7f73e74b
Claudio Alves Santurian <claudiosanturian11@gmail.com>
2026-07-28 20:04:11 -0300

    Adicionando tentativas na hora do login

--- main.py
+++ main.py
@@ -10,35 +10,43 @@ if usuario_cadastrado is None:
 else:
     print('---LOGIN---')
 
-    nome_usuario = input('Digite seu nome de usuário: ')
-    senha_usuario = input('Digite sua senha: ')
+    tentativas = 0
+    login_correto = False
+    while tentativas < 3 and login_correto == False:
+        nome_usuario = input('Digite seu nome de usuário: ')
+        senha_usuario = input('Digite sua senha: ')
 
-    if nome_usuario == usuario_cadastrado and senha_usuario == senha_cadastrada:
-        print('LOGIN FEITO COM SUCESSO!')
-        print('\n--- Bem vindo ao sistema de despesas ---')
-        while True:
-            print('\n--- MENU PRINCIPAL---')
-            print('1. Adicionar Despesa')
-            print('2. Listar Despesas')
-            print('3. Ver total')
-            print('4. Fechar Lista do Mês')
-            print('0. Sair')
+        if nome_usuario == usuario_cadastrado and senha_usuario == senha_cadastrada:
+            login_correto = True
+            print('LOGIN FEITO COM SUCESSO!')
+            print('\n--- Bem vindo ao sistema de despesas ---')
+            while True:
+                print('\n--- MENU PRINCIPAL---')
+                print('1. Adicionar Despesa')
+                print('2. Listar Despesas')
+                print('3. Ver total')
+                print('4. Fechar Lista do Mês')
+                print('0. Sair')
 
-            opcao = input('Digite a opção desejada: ')
+                opcao = input('Digite a opção desejada: ')
             
-            if opcao == '1':
-                adicionar_despesa()
-            elif opcao == '2':
-                listar_despesas()
-            elif opcao == '3':
-                total_despesas()
-            elif opcao == '4':
-                fechar_mes()
-            elif opcao == '0':
-                print('Saindo...')
-                break
-            else:
-                print('Opção Inválida!')
+                if opcao == '1':
+                    adicionar_despesa()
+                elif opcao == '2':
+                    listar_despesas()
+                elif opcao == '3':
+                    total_despesas()
+                elif opcao == '4':
+                    fechar_mes()
+                elif opcao == '0':
+                    print('Saindo...')
+                    break
+                else:
+                    print('Opção Inválida!')
 
-    else:
-        print('Usuário ou senha incorretos!')
+        else:
+            tentativas += 1
+            print(f'Usuário ou senha incorretos! Tentativas restantes: {3 - tentativas}')
+
+    if login_correto == False:
+        print('Números de tentativas excedido. Encerrando...')
