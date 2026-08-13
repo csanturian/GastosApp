53f4048cb407381c6f41d7e3ca8cbd4855957f52
Claudio Alves Santurian <claudiosanturian11@gmail.com>
2026-08-13 19:28:29 -0300

    ajustando linhas de print

--- senha.py
+++ senha.py
@@ -7,7 +7,7 @@ def validar_usuario(usuario):
         if len(usuario) > 15: 
               return('Nome de usuario longo demais!')
         if not re.search ('[a-z]', usuario) : 
-              return 'Falta letra minúscula'
+              return 'Falta letra minúscula!'
         if not re.search ('[A-Z]', usuario) : 
               return('Falta letra maiúscula!')
         return 'OK'
@@ -16,7 +16,7 @@ def validar_senha(senha_informada):
         if len(senha_informada) < 8: 
               return('Senha Curta!')
         if not re.search ('[a-z]', senha_informada) : 
-              return 'Falta letra minúscula'
+              return 'Falta letra minúscula!'
         if not re.search ('[A-Z]', senha_informada) : 
               return('Falta letra maiúscula!')
         if not re.search ('[0-9]', senha_informada) : 
