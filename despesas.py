931368869ab56cabff3d94299923e8ddc6570072
Claudio Alves Santurian <claudiosanturian11@gmail.com>
2026-08-14 02:05:23 -0300

    adicionando formatação de valor .2f

--- despesas.py
+++ despesas.py
@@ -7,7 +7,11 @@ def calcular_total():
 
 def adicionar_despesa():
     nome = input('Qual o nome da despesa adicionada: ')
-    valor = float(input('Qual o valor da despesa: '))
+    try:
+        valor = float(input('Qual o valor da despesa: '))
+    except ValueError:
+         print("Valor Inválido!")
+         return
     categoria = input('Qual a categoria da despesa: ')
 
     despesas.append((nome, valor, categoria))
@@ -20,11 +24,11 @@ def listar_despesas():
     
     print('\n--- Suas Despesas ---')
     for nome, valor, categoria in despesas:
-        print(f'{nome} - R${valor} - {categoria}')
+        print(f'{nome} - R${valor:.2f} - {categoria}')
         
 def total_despesas():
     total = calcular_total()
-    print(f'O total gasto foi: R${total}')
+    print(f'O total gasto foi: R${total:.2f}')
 
 def fechar_mes():
     global despesas
@@ -39,7 +43,7 @@ def fechar_mes():
             total = calcular_total()
 
             print('\n--- FECHAMENTO DO MÊS ---')
-            print(f'Total gasto no mês: R${total}')
+            print(f'Total gasto no mês: R${total:.2f}')
 
             data_atual = datetime.now().strftime('%m_%Y')
             nome_arquivo = f'relatorio_{data_atual}.txt'
@@ -51,12 +55,12 @@ def fechar_mes():
                            subtotal_categoria[categoria] = subtotal_categoria[categoria] + valor
                       else:
                            subtotal_categoria[categoria] = valor
-                      arquivo.write(f'{nome} - R${valor} - {categoria}\n')
-                 arquivo.write(f'\nTotal gasto no mês: R${total}\n')
+                      arquivo.write(f'{nome} - R${valor:.2f} - {categoria}\n')
+                 arquivo.write(f'\nTotal gasto no mês: R${total:.2f}\n')
 
                  arquivo.write('\n-----RESUMO POR CATEGORIA-----\n')
                  for categoria, valor in subtotal_categoria.items():
-                        arquivo.write(f'{categoria}: R${valor}\n')
+                        arquivo.write(f'{categoria}: R${valor:.2f}\n')
             despesas = []
 
             print('Novo mês iniciado! Despesas zeradas.')
